598dd91d8237f6c5601b32777a4ad5b588d60525
Marouan Ajtirah <marouan_39@hotmail.com>
2023-06-06 22:26:04 +0200

    ee

--- database/migrations/2023_05_14_145412_fields.php
+++ database/migrations/2023_05_14_145412_fields.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->json('items')->nullable();
+            $table->json('notifications')->nullable(); // Add notifications field
             $table->rememberToken();
             $table->timestamps();
         });
