0f841bb07fd212ad0c030ea02805c90778d7e277
Marouan Ajtirah <marouan_39@hotmail.com>
2023-06-09 11:50:49 +0200

    ee

--- config/scribe.php
+++ config/scribe.php
@@ -191,7 +191,7 @@ return [
          * The base URL for the API tester to use (for example, you can set this to your staging URL).
          * Leave as null to use the current app URL (config(app.url)).
          */
-        'base_url' => null,
+        'base_url' => 'https://orca-app-ik7qo.ondigitalocean.app/',
 
         /**
          * Fetch a CSRF token before each request, and add it as an X-XSRF-TOKEN header. Needed if you're using Laravel Sanctum.
@@ -258,8 +258,7 @@ This documentation aims to provide all the information you need to work with our
 
 <aside>As you scroll, you'll see code examples for working with the API in different programming languages in the dark area to the right (or as part of the content on mobile).
 You can switch the language used with the tabs at the top right (or from the nav menu at the top left on mobile).</aside>
-INTRO
-    ,
+INTRO,
 
     /*
      * Example requests for each endpoint will be shown in each of these languages.
