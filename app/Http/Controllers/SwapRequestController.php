cd97741c01794a040859d54f41a78faafd7bd3c0
Marouan Ajtirah <marouan_39@hotmail.com>
2023-06-13 23:59:20 +0200

    fixing post request

--- app/Http/Controllers/SwapRequestController.php
+++ app/Http/Controllers/SwapRequestController.php
@@ -10,6 +10,26 @@ use App\Models\SwapRequest;
 
 class SwapRequestController extends Controller
 {
+
+    public function store(Request $request)
+    {
+        // Validate the request data
+        $validatedData = $request->validate([
+            'sender_id' => 'required|exists:users,id',
+            'receiver_id' => 'required|exists:users,id',
+            'item_id' => 'required|exists:items,id',
+        ]);
+
+        // Create the swap request
+        $swapRequest = SwapRequest::create($validatedData);
+
+        // Send notifications to the receiver
+        // You can implement your notification logic here, such as sending emails or push notifications to the receiver
+
+        // Return a response indicating success
+        return response()->json(['message' => 'Swap request sent successfully'], 200);
+    }
+
     public function receivedSwapRequests($userId)
     {
         $user = User::findOrFail($userId);
